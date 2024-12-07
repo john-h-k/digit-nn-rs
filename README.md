9b4d2dc1f6970ecbd5b4075e9fc8dd2316c37c77
John Kelly <johnharrykelly@gmail.com>
2024-12-07 15:29:15 +0000

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# MNIST Neural net in Rust
+
+Why not?
+
+Run `fmt.py` against the pickle file from here:
+https://github.com/mnielsen/neural-networks-and-deep-learning/blob/master/data/mnist.pkl.gz
+
+Then you can run the net
--- src/main.rs
+++ src/main.rs
@@ -50,11 +50,11 @@ fn main() {
 
     let (training_data, validation_data, test_data) = mnist::load_data_wrapper();
 
-    // let mut net = Network::new(vec![784, 30, 10]);
-    // eprintln!("Training");
-    // net.stochastic_gradient_descent(training_data, 30, 10, 3.0, Some(test_data));
+    let mut net = Network::new(vec![784, 30, 10]);
+    eprintln!("Training");
+    net.stochastic_gradient_descent(training_data, 100, 10, 3.0, Some(test_data));
 
-    let mut net = Network::load("model", vec![784, 30, 10]).unwrap();
+    // let mut net = Network::load("model", vec![784, 30, 10]).unwrap();
 
     let res = net.evaluate(&[data])[0];
 
